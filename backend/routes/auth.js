916fabe69f848dbec27d633b823da8f830ac8979
Yahav Corcos <110262882+ycorcos1@users.noreply.github.com>
2025-02-15 17:36:47 -0500

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,12 +6,19 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const router = express.Router();
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // **Signup Route**
