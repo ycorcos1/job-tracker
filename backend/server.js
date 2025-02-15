e4f6ce1b665393c5ec1ee970f7811e9a9556358f
Yahav Corcos <110262882+ycorcos1@users.noreply.github.com>
2025-02-15 16:56:52 -0500

    Updated Backend

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Test route
